0e6321505d14c7858ec2d02d83d0d67f3dc6945c
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-03-14 17:09:13 -0300

    feat: Created two buttons for Products and Cart lists and a useState to handle the click

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductsList from "./ProductsList";
+import Cart from "./Cart";
 
 const Store = () => {
+
+  const [elements, setElements] = useState('products');
+
   return (
     <>
       <h1>My store</h1>
+      <button onClick={() => setElements('products')}>Products</button>
+      <button onClick={() => setElements('cart')}>Cart</button>
       <ul>
-        <ProductsList />
+        {elements === 'products' && <ProductsList />}
+        {elements === 'cart' && <Cart />}
       </ul>
     </>
   )
