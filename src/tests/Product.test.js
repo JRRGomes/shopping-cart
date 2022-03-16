c21628c0a389059ded91797b25d4490f6f71399c
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-03-16 15:28:19 -0300

    feat: Created Product component test to check if its renreding the add to cart button

--- src/tests/Product.test.js
+++ src/tests/Product.test.js
@@ -3,12 +3,17 @@ import { render, screen } from "@testing-library/react";
 import Product from "../components/Product";
 
 describe("Product", () => {
+  const product = {
+      name: 'Product-1',
+      description: 'this is the first product'
+    };
   it("should render the products first object name and description", () => {
-    const product = {
-        name: 'Product-1',
-        description: 'this is the first product'
-      };
     render(<Product product={product} />)
     expect(screen.getByText('Product-1: this is the first product' )).toBeInTheDocument();
+  });
+
+  it("should render a add to cart button", () => {
+    render(<Product product={product} />)
+    expect(screen.getByRole('button', { name: 'Add to cart' })).toBeInTheDocument();
   });
 });
