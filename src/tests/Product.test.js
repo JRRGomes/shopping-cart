78f4ba76e2e6342324f6167ed154318c6d9df82f
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-03-16 15:41:18 -0300

    feat: Created test for Product component to check if the addToCart function is been called when clicking the button

--- src/tests/Product.test.js
+++ src/tests/Product.test.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
 import Product from "../components/Product";
 
+const product = {
+    name: 'Product-1',
+    description: 'this is the first product'
+  };
+
 describe("Product", () => {
-  const product = {
-      name: 'Product-1',
-      description: 'this is the first product'
-    };
   it("should render the products first object name and description", () => {
     render(<Product product={product} />)
     expect(screen.getByText('Product-1: this is the first product' )).toBeInTheDocument();
@@ -15,5 +17,15 @@ describe("Product", () => {
   it("should render a add to cart button", () => {
     render(<Product product={product} />)
     expect(screen.getByRole('button', { name: 'Add to cart' })).toBeInTheDocument();
+  });
+});
+
+describe("When add to cart button is clicked", () => {
+  it("should dispatch addToCart function", () => {
+    const addToCart = jest.fn();
+    render(<Product product={product} addToCart={addToCart}/>);
+    userEvent.click(screen.getByRole('button', { name: 'Add to cart' }))
+
+    expect(addToCart).toHaveBeenCalled();
   });
 });
