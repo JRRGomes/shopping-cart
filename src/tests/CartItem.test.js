ecc26b4095fb5084879cf39538b55f7f43ca721c
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-03-17 15:30:46 -0300

    Created tests for CartItem to check if the the remove from cart button is been rendered. Also created test to check if when clicked the button is calling the removeFromCart function once and if its been called with item.id as a prop

--- src/tests/CartItem.test.js
+++ src/tests/CartItem.test.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CartItem from "../components/CartItem";
 
 const item = {
+    id: 1,
     name: 'Product-1',
     description: 'this is the first product'
   };
@@ -10,6 +12,23 @@ const item = {
 describe("CartItem", () => {
   it("should render the cart item name and description", () => {
     render(<CartItem item={item} />)
-    expect(screen.getByText('Product-1: this is the first product' )).toBeInTheDocument();
+    expect(screen.getByText('Product-1: this is the first product')).toBeInTheDocument();
+  });
+
+  it("should render a remove from cart button", () => {
+    const removeFromCart = jest.fn();
+    render(<CartItem item={item} removeFromCart={removeFromCart}/>)
+    expect(screen.getByRole('button', { name: 'Remove from cart' })).toBeInTheDocument();
+  });
+
+  describe("When remove from cart button is clicked", () => {
+    it("should dispatch addToCart function", () => {
+      const removeFromCart = jest.fn();
+      render(<CartItem item={item} removeFromCart={removeFromCart}/>);
+      userEvent.click(screen.getByRole('button', { name: 'Remove from cart' }))
+  
+      expect(removeFromCart).toHaveBeenCalledTimes(1);
+      expect(removeFromCart).toHaveBeenCalledWith(item.id);
+    });
   });
 });
