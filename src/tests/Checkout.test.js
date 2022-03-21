27b77e2cb9815d03210b60ffc3c2fee4b54abfcf
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-03-21 18:12:34 -0300

    feat: Created test for Checkout component to check if its rendering payment method and order summary when clicking on Next button

--- src/tests/Checkout.test.js
+++ src/tests/Checkout.test.js
@@ -1,14 +1,32 @@
 import Checkout from "../components/Checkout";
 import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 
 describe("Checkout", () => {
   it("should render Checkout text ", () => {
     render(<Checkout />);
     expect(screen.getByText('Checkout')).toBeInTheDocument();
   });
-  
+
   it("should render Shipping address text ", () => {
     render(<Checkout />);
     expect(screen.getAllByText('Shipping address')[0]).toBeInTheDocument();
+  });
+
+  describe("When clicking in Next", () => {
+    it("should render payment method text ", () => {
+      render(<Checkout />);
+      userEvent.click(screen.getByRole('button', { name: 'Next' }));
+      expect(screen.getByText('Payment method')).toBeInTheDocument();
+    });
+  });
+
+  describe("When clicking in Next twice", () => {
+    it("should render order summary text ", () => {
+      render(<Checkout />);
+      userEvent.click(screen.getByRole('button', { name: 'Next' }));
+      userEvent.click(screen.getByRole('button', { name: 'Next' }));
+      expect(screen.getByText('Order summary')).toBeInTheDocument();
+    });
   });
 });
