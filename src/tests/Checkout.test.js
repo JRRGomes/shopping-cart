ecb4c0996dda6e9bc73590c5bb6e18d1feb01115
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-03-25 10:38:18 -0300

    feat: Created two functions on Checkout test to fill the Address and Payment form. Created tests to check if the Next button is disabled on both pages and if its rendering the pages after the forms are filled and the Next button is pressed

--- src/tests/Checkout.test.js
+++ src/tests/Checkout.test.js
@@ -2,6 +2,25 @@ import Checkout from "../components/Checkout";
 import { render, screen } from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
 
+const fillAdressForm = () => {
+  userEvent.type(screen.getByRole('textbox', { name: /first name/i }), 'teste')
+  userEvent.type(screen.getByRole('textbox', { name: /last name/i }), 'teste')
+  userEvent.type(screen.getByRole('textbox', { name: /address line 1/i }), 'teste')
+  userEvent.type(screen.getByRole('textbox', { name: /zip \/ postal code/i }), '1234')
+  userEvent.type(screen.getByRole('textbox', { name: /city/i }), 'teste')
+  userEvent.click(screen.getByRole('button', { name: /country/i }));
+  userEvent.click(screen.getByRole('option', { name: /brazil/i }));
+  userEvent.click(screen.getByRole('button', { name: /state\/province\/region \*/i }));
+  userEvent.click(screen.getByRole('option', { name: /maranhao/i }));
+}
+
+const fillPaymentForm = () => {
+  userEvent.type(screen.getByRole('textbox', { name: /name on card/i }), 'teste')
+  userEvent.type(screen.getByRole('textbox', { name: /card number/i }), 'teste')
+  userEvent.type(screen.getByRole('textbox', { name: /expiry date/i }), '1234')
+  userEvent.type(screen.getByRole('textbox', { name: /cvv/i }), '1234')
+}
+
 describe("Checkout", () => {
   it("should render Checkout text ", () => {
     render(<Checkout />);
@@ -19,25 +38,34 @@ describe("Checkout", () => {
   });
 
   describe("When clicking in Next", () => {
-    beforeEach(()=>{
-      render(<Checkout />);
-      userEvent.type(screen.getByRole('textbox', { name: /first name/i }), 'teste')
-      userEvent.type(screen.getByRole('textbox', { name: /last name/i }), 'teste')
-      userEvent.type(screen.getByRole('textbox', { name: /address line 1/i }), 'teste')
-      userEvent.type(screen.getByRole('textbox', { name: /zip \/ postal code/i }), '1234')
-      userEvent.type(screen.getByRole('textbox', { name: /city/i }), 'teste')
-      userEvent.click(screen.getByRole('button', { name: /country/i }));
-      userEvent.click(screen.getByRole('option', { name: /brazil/i }));
-      userEvent.click(screen.getByRole('button', { name: /state\/province\/region \*/i }));
-      userEvent.click(screen.getByRole('option', { name: /maranhao/i }));
-      userEvent.click(screen.getByRole('button', { name: 'Next' }));
-    });
-
     it("should render payment method text ", () => {
-      expect(screen.getByText('Payment method')).toBeInTheDocument();  
-
+      render(<Checkout />);
+      fillAdressForm()
       userEvent.click(screen.getByRole('button', { name: 'Next' }));
-      expect(screen.getByText('Order summary')).toBeInTheDocument();
+
+      expect(screen.getByText('Payment method')).toBeInTheDocument();  
+    });
+  });
+
+  describe("When fill in the AdressForm and click in Next", () => {
+    it("should have a disabled Next button ", () => {
+      render(<Checkout />);
+      fillAdressForm()
+      userEvent.click(screen.getByRole('button', { name: 'Next' }));
+
+      expect(screen.getByRole('button', { name: 'Next' })).toBeDisabled();
+    });
+  });
+
+  describe("When clicking in Next again", () => {
+    it("should render order summary text ", () => {
+      render(<Checkout />);
+      fillAdressForm()
+      userEvent.click(screen.getByRole('button', { name: 'Next' }));
+      fillPaymentForm()
+      userEvent.click(screen.getByRole('button', { name: 'Next' }));
+
+      expect(screen.getByText('Order summary')).toBeInTheDocument();  
     });
   });
 
