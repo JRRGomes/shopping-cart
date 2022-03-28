6b1f3b6d96dba27ff7bebd50538836e3080b873a
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-03-28 12:58:05 -0300

    feat: Created a product.json to keep the products info. Added a price to each product. Passed the cartItem list to review as a prop do display it there. Added a total const as a sum of each product price

--- src/components/Checkout/Review.js
+++ src/components/Checkout/Review.js
@@ -5,31 +5,7 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Grid from '@mui/material/Grid';
 
-const products = [
-  {
-    name: 'Product 1',
-    desc: 'A nice thing',
-    price: '$9.99',
-  },
-  {
-    name: 'Product 2',
-    desc: 'Another thing',
-    price: '$3.45',
-  },
-  {
-    name: 'Product 3',
-    desc: 'Something else',
-    price: '$6.51',
-  },
-  {
-    name: 'Product 4',
-    desc: 'Best thing of all',
-    price: '$14.11',
-  },
-  { name: 'Shipping', desc: '', price: 'Free' },
-];
-
-export default function Review({ formValues }) {
+export default function Review({ formValues, cartItems }) {
 
   const maskedCardNumber = `xxxx-xxxx-xxxx-${formValues.cardNumber.slice(-4)}`
   const payments = [
@@ -39,23 +15,26 @@ export default function Review({ formValues }) {
     { name: 'Expiry date', detail: formValues.expDate },
   ];
 
+  const total = cartItems.cartItems.map((item) => Number(item.price))
+    .reduce((item1, item2) => item1 + item2)
+
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
         Order summary
       </Typography>
       <List disablePadding>
-        {products.map((product) => (
+        {cartItems.cartItems.map((product) => (
           <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
-            <ListItemText primary={product.name} secondary={product.desc} />
-            <Typography variant="body2">{product.price}</Typography>
+            <ListItemText primary={product.name} secondary={product.description} />
+            <Typography variant="body2">${product.price}</Typography>
           </ListItem>
         ))}
 
         <ListItem sx={{ py: 1, px: 0 }}>
           <ListItemText primary="Total" />
           <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
-            $34.06
+            ${total}
           </Typography>
         </ListItem>
       </List>
